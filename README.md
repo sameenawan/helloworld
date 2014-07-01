4df82f868637869b4f9685e5d88cd528c5a85e3e
Sameen Awan <awan@g.harvard.edu>
2014-06-30 23:51:06 -0400

    P1 Project file

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Hello World
+# Project P1
 
-Just for practice
 
-Sameen Awan
+## Live URL
+<http://helloworldsameen.gopagoda.com/>
+
+
+## Description
+A simple webpage loaded on to pagoda. This project displays that I understand the material so far and can set up a project framework to work in. 
+
+## Details for teaching team
+Public site, no login required.
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>Sameen Awan</title>
+<link href="css/piinfinity.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<LINK REL="SHORTCUT ICON" HREF="images/favicon.ico">
+<meta name="description" content="A little about Sameen Awan" />
+<meta name="keywords" content="sameen, awan" />
+<meta name="Robots" content="index,follow" />
+</head>
+
+
+<body>
+<div id="header">
+  
+<img src="images/sameen.jpg"  alt="Sameen Awan" >
+
+</div>
+<div id="content">
+   <table>
+    <tr>
+      <td>
+        
+<h2 class="mheading">Sameen Awan</h2><p class="textt">
+
+<h3>In 2+ sentences describe who you are and what you do.</h3>
+ 
+Hi, my full name is Dur-E-Sameen Awan, but I go by the name Sameen. I am from Canada, currently working for the government of Canada as a data analyst. This is my first time visiting Boston, and Harvard University. It's been 6+ years since I last attended a university. 
+ 
+
+<h3>Why are you taking this course?</h3>
+ 
+ At work, I took the initiative to build a website for the Ontario Government, visualizing crime rates and traffic offences within each city in Ontario. Part of that project involved creating over 3017 graphs that would need to be updated on a monthly basis. For this reason, I am taking this dynamic web development course, so I can complete my project at work.
+ 
+
+<h3>What previous experience do you have with HTML/CSS?</h3>
+ 
+My experience with HTML was revived lately when I  took on the website project at my current job. I have self learnt CSS and HTML during that time. I have also taken a course last semester on HTML/CSS.
+ 
+
+
+
+
+
+
+
+
+<h3>What previous experience do you have with programming?</h3>
+ <p>
+I don't really have much experience with programming. I recently have started playing around with javascript, since I decided to use HighCharts application to create animated graphs for the government website. I now know enoug to add javascript modules to a website and be able to customize them.
+ </P>
+
+
+<h3>Have you worked with PHP before? If so, to what extent?</h3>
+ <p>
+Recently, I did a little bit of work with PHP just using the include command. so to keep headers and footers consistent on all pages, use PHP INCLUDE command and post all header and footer content in separate php documents.
+ </p>
+
+<h3>Are you working on a Mac, PC, or some other platform?</h3>
+ <p>
+I am working on a PC. I have never really used a Mac before. 
+ </p>
+
+<h3>Anything else we should know?</h3>
+ <p>
+Well, I am really excited to learn everything in this class especially since I will have to apply it right away when I get back to work. My manager is really depending on me. I am hoping to learn how to use javascript modules like high charts and link that to my MYSQL database, and be able to create dynamic graphs (all 3017 of them).
+ </p>
+
+
+</td>
+
+      <td><img src="images/sameen.jpg" alt="Pi Infinity Tutoring Services serving the future."></td>
+    </tr>
+
+
+        
+  </table>
+</div>
+<div id="footer" >&copy; 2014 Piinfinitytutoringservice.com -  all rights reserved. &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;   Sameen Awan</div>
+</body>
+</html>
